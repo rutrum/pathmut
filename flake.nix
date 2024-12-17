3466ef2187e9b57bd4a35a3ea9fad6d15d76dd9c
rutrum <dave@rutrum.net>
2024-12-16 19:02:19 -0500

    Cleanup flake

--- flake.nix
+++ flake.nix
@@ -10,20 +10,16 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
 
-    overrides = pkgs.lib.importTOML ./rust-toolchain.toml;
+    #overrides = pkgs.lib.importTOML ./rust-toolchain.toml;
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       inherit name;
       buildInputs = with pkgs; [
         rustup
-        #rustPlatform
         just
         watchexec
       ];
-
-      # shouldn't be required?
-      #RUSTC_VERSION = overrides.toolchain.channel;
     };
   };
 }
